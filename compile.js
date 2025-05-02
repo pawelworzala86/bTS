77e1d3e979302191aba7eb42860df6a29faf01be
Pawel Worzala <pawelworzala86@gmail.com>
2025-05-02 16:18:55 +0200

    auto save

--- compile.js
+++ compile.js
@@ -2,4 +2,9 @@ const fs = require('fs')
 
 let code = fs.readFileSync('./source/test.js').toString()
 
-fs.writeFileSync('./cache/test.asm',code)
+
+let frame = fs.readFileSync('./frame/cmd.inc').toString()
+frame = frame.replace('{{CODE}}',code)
+frame = frame.replace('{{DATA}}','')
+
+fs.writeFileSync('./cache/test.asm',frame)
