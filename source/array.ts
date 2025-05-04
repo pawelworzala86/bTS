0bd9401baf886d9082c46f6e8e3b0616f9a6e8cd
Pawel Worzala <pawelworzala86@gmail.com>
2025-05-04 15:07:17 +0200

    auto save

---
+++ source/array.ts
@@ -0,0 +1,19 @@
+import * as imp1 from './imp1.ts'
+import * as msvcrt from './msvcrt.ts'
+
+//export declare function printf(...args:any[]):void//msvcrt
+
+/*class OBJ{
+    fieldA:number = 12
+    fieldB:number = 13
+}*/
+
+//let objA:imp1.OBJ = new imp1.OBJ()
+
+//let valA:number = 123
+
+function main(){
+    msvcrt.printf("ok ")
+
+    msvcrt.printf("end ")
+}
