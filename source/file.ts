ba70746c6d38d7c66cd28b793e3e39c7e24951ff
Pawel Worzala <pawelworzala86@gmail.com>
2025-05-04 20:18:30 +0200

    auto save

--- source/file.ts
+++ source/file.ts
@@ -1,5 +1,27 @@
+import * as kernel from './kernel.ts'
 import * as msvcrt from './msvcrt.ts'
 
-export function readFile(fileName:string){
-    msvcrt.printf(" %s ", fileName)
+export let handle:number = 0
+export let fsize:number = 0
+export let buffor:number = 0
+
+let fileName:string = 'test.txt'
+
+export function readFile(fileNameA:string){
+
+    msvcrt.printf(" %s ",fileName)
+    
+    kernel.CreateFileA(fileName, GENERIC_READ,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL, 0)
+    mov handle, rax
+    msvcrt.printf(" h %i ",handle)
+
+    kernel.GetFileSize(handle, 0)
+    mov fsize, rax
+    msvcrt.printf(" sz %i ",fsize)
+
+    msvcrt.malloc(fsize)
+    mov buffor, rax
+    kernel.ReadFile(handle, buffor, fsize, 0, 0)
+    //printf('%s',buffor)
+    kernel.CloseHandle(handle)
 }
---
+++ source/kernel.ts
@@ -0,0 +1,4 @@
+export declare function CreateFileA(...args:any[]):void//kernel
+export declare function GetFileSize(...args:any[]):void//kernel
+export declare function ReadFile(...args:any[]):void//kernel
+export declare function CloseHandle(...args:any[]):void//kernel
