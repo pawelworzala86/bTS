ebf2a55cf4f30c43de008d058fe25e7f7d043de4
Pawel Worzala <pawelworzala86@gmail.com>
2025-05-05 15:08:11 +0200

    auto save

--- source/kernel.ts
+++ source/kernel.ts
@@ -1,4 +1,4 @@
-export declare function CreateFileA(...args:any[]):void//kernel
-export declare function GetFileSize(...args:any[]):void//kernel
+export declare function CreateFileA(...args:any[]):any//kernel
+export declare function GetFileSize(...args:any[]):any//kernel
 export declare function ReadFile(...args:any[]):void//kernel
 export declare function CloseHandle(...args:any[]):void//kernel
