4197f69026315f974c0f36809fa432affc4883d1
Pawel Worzala <pawelworzala86@gmail.com>
2025-06-20 19:46:46 +0200

    auto save

--- asm/str.asm
+++ asm/str.asm
@@ -8,7 +8,7 @@ entry start
 
 section '.data' data readable writeable
     textA db "example", 0
-    textB db "aaaaaaa", 0
+    textB db "am", 0
 
 section '.text' code readable executable
 
@@ -70,6 +70,45 @@ StrCopy:
 ret
 
 
+StrPos:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 8*1
+
+    mov rsi, [rbp + 16]           ; wskaźnik na string (argument 1)
+    mov rdi, [rbp + 24]           ; wskaźnik na string (argument 2)
+
+    ;invoke printf, '%s', rdi
+
+    xor rdx, rdx           ; licznik długości = 0
+    xor r9, r9           ; licznik długości = 0
+
+.while:
+    xor r9, r9
+    mov al, [rsi + rdx]
+    .secStr:
+    inc r9
+    mov bl, [rdi + r9]
+    ;invoke printf, '%c', bl
+    cmp bl, 0
+    je .end
+    cmp bl, al
+    je .secStr
+    ;xor r9, r9
+    cmp al, 0
+    je .end
+    inc rdx
+    jmp .while
+.end:
+    ; wynik długości jest w rdx
+
+    mov rax, rdx
+
+    mov rsp, rbp
+    pop rbp
+ret
+
+
 start:
     sub rsp, 8
 
@@ -83,6 +122,13 @@ start:
     call StrCopy
     add rsp, 8
     invoke printf, '%s', rax
+
+    push 0
+    push textB
+    push textA
+    call StrPos
+    add rsp, 8*3
+    invoke printf, '%i', rax
 
     invoke ExitProcess, 0  ; Zakończ program
 
