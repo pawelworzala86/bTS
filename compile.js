0931d75be8eb17b91b77aee25eedbdc8f63d41de
Pawel Worzala <pawelworzala86@gmail.com>
2025-07-19 16:09:58 +0200

    auto save

--- compile.js
+++ compile.js
@@ -506,8 +506,8 @@ function Compile(file,remdir=''){
 
     r(/^(.*)\b(this\.[a-zA-Z0-9\_]+)\[([0-9]+)\]/gm,'lea rsi,$2\n$1 qword[rsi + $3*8]')
     r(/^(.*)\b(this\.[a-zA-Z0-9\_]+)\[([a-zA-Z0-9\_]+)\]/gm,'lea rsi,$2\nmov rdi,[$3]\n$1 qword[rsi + rdi*8]')
-    r(/^(.*)\b([a-zA-Z0-9\_]+)\[([0-9]+)\]/gm,'lea rsi,$2\n$1 qword[rsi + $3*8]')
-    r(/^(.*)\b([a-zA-Z0-9\_]+)\[([a-zA-Z0-9\_]+)\]/gm,'lea rsi,$2\nmov rdi,[$3]\n$1 qword[rsi + rdi*8]')
+    r(/^(.*)\b([a-zA-Z0-9\_]+)\[([0-9]+)\]/gm,'mov rsi,$2\n$1 qword[rsi + $3*8]')
+    r(/^(.*)\b([a-zA-Z0-9\_]+)\[([a-zA-Z0-9\_]+)\]/gm,'mov rsi,$2\nmov rdi,[$3]\n$1 qword[rsi + rdi*8]')
 
     fs.writeFileSync('./cache/ba2.js',source)
 
