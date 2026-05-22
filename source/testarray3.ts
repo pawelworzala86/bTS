54a592deb9de0a8350cdc3dc084cc46277d5a569
Paweł Worzała <pawelworzala86@gmail.com>
2026-05-22 18:41:05 +0200

    test array 3

--- source/testarray3.ts
+++ source/testarray3.ts
@@ -1,17 +1,27 @@
 import * as msvcrt from './system/msvcrt.ts'
 
 let arrayRecordSize = 8
+let ls:number = 0
+let rs:number = 0
 
 class ARRAY{
     array:number = 0
+    size:number = 0
     total:number = 0
+    constructor(){
+        this.array = msvcrt.malloc(8)
+    }
     push(value:number){
         let index:number = 0
         index = this.total
         this.total++
-        let size:number = 0
-        size = this.total * arrayRecordSize//int
-        this.array = msvcrt.malloc(size)
+        ls = this.total
+        rs = this.size
+        if(ls==rs){
+            let size:number = 0
+            size = this.total * arrayRecordSize//int
+            this.array = msvcrt.realloc(size)
+        }
         this.array[index] = value
     }
     get(index:number){
@@ -25,9 +35,10 @@ function main(){
     msvcrt.printf("ok ")
 
     arra.push(111)
+    arra.push(222)
 
     let value:number = 0
-    value = arra.get(0)
+    value = arra.get(1)
     msvcrt.printf(" %i ",value)
 
 
